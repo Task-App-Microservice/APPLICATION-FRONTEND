1483e79456bd514e4630b33bc11a2c0df443a3ed
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2025-03-26 07:10:12 +0000

    asas

--- src/config/axios/axios.instance.ts
+++ src/config/axios/axios.instance.ts
@@ -5,6 +5,9 @@ const createAxiosInstance = (): AxiosInstance => {
     const AxiosInstance = axios.create({
         baseURL: process.env.NEXT_PUBLIC_API_URL,
         timeout: 50000,
+        headers: { 
+          'Content-Type': 'application/json',
+        } 
     });
 
     return AxiosInstance; 
