f391fb4cf045fb5b674a86f52a7b031d743c9344
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2025-03-26 00:08:05 +0000

    install dependecies

--- src/providers/index.provider.tsx
+++ src/providers/index.provider.tsx
@@ -1,9 +1,16 @@
+"use client"
 import React from 'react'
-const Providers = ({children}:{children:React.ReactNode}) => {
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        <>
-            {children} 
-        </>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
     )
 }
 
