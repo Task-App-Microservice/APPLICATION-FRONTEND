379b5729f1aaa57279535afde0c2988f0ac8f6d6
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2025-03-29 08:12:08 +0000

    dd

--- src/app/(pages)/(app)/project/[project]/page.tsx
+++ src/app/(pages)/(app)/project/[project]/page.tsx
@@ -1,4 +1,5 @@
 import ProjectLoyout from '@/components/features/layout/project/index.loyout'
+import ProjectTasks from '@/components/features/layout/project/task/project-tasks'
 import React from 'react'
 interface Params{
   params:Promise<{
@@ -8,7 +9,7 @@ interface Params{
 const Page = async({params}:Params) => {
   return (
     <ProjectLoyout projectUuid={(await params).project}>
-      <ProjectTasks />
+      <ProjectTasks projectUuid={(await params).project} />
     </ProjectLoyout>
   )
 }
